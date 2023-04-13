66214335b687b77f929322c5e909a1821cffe4af
Amirali-fakhredin <130482214+Amirali-fakhredin@users.noreply.github.com>
2023-04-13 06:40:20 -0700

    Update virus.h

--- virus.h
+++ virus.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "genome.h"
+#include "animal.h"
+#include <vector>
+#include <iostream>
 class virus:genome
 {
 public:
 	virus();
-	bool CheckHarmfulVirus();
+	bool CheckHarmfulVirus(vector <string> strings);
 	void print();
+	
 };
-
